15296fe0acef12c110c03f4bd6965601b47abe6c
Dare Jamiu <jammy190@gmail.com>
2023-04-01 22:17:09 +0100

    drum-kit day one

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[datakey="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[datakey="${e.keyCode}"]`);
+    if(!audio) return; //stop function from running
+    audio.currentTime = 0;
+    audio.play();
+   console.log(audio)
+});
