f7a761df2fd9eb60425fd968b568953d2951af81
Dare Jamiu <jammy190@gmail.com>
2023-04-05 10:17:54 +0100

    final commit

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[datakey="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[datakey="${e.keyCode}"]`);
-    if(!audio) return; //stop function from running
-    audio.currentTime = 0;
-    audio.play();
-   console.log("keeeaay");
-});
